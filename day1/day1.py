dd5a7b46656888373fff10d4949bd267796e468e
Harrison Gertler <harrisongertler@mac.com>
2023-12-04 10:57:58 -0500

    Finished answer for part 1

--- day1/day1.py
+++ day1/day1.py
@@ -4,11 +4,26 @@ import sys, os
 
 def usage():
     print(f"Usage: {sys.argv[0]} [INPUT FILE]")
-    print("Find the Elf carrying the most Calories.\n")
+    print("Find the sum of each calibration value.\n")
     print("\t-h\tPrint this help message\n")
 
-def main():
-    pass
+def main(input_file_name):
+    lines = []
+    # Read lines from input
+    with open(input_file_name) as input_file:
+        lines = input_file.readlines()
+    
+    # Collect calibration values
+    total = 0
+    for line in lines:
+        # Calculate first and last digit, then add number to total
+        numerals = list( filter( lambda i: i.isnumeric(), line ) )
+        first = int( numerals[0] )
+        last = int( numerals[-1] )
+        num = (first * 10) + last
+        total += num
+    
+    print(f"The sum of each calibration value is {total}")
 
 if __name__ == "__main__":
     # Check args:
