d063b556eea5fde45fcbcc189b0a808533a2ac95
Harrison Gertler <harrisongertler@mac.com>
2023-12-04 11:39:47 -0500

    Finished part 2

--- day1/day1.py
+++ day1/day1.py
@@ -1,12 +1,22 @@
 #! /usr/bin/env python3
 
 import sys, os
+import re
 
 def usage():
     print(f"Usage: {sys.argv[0]} -p [PART_NUMBER] [INPUT_FILE]")
     print("Find the sum of each calibration value.\n")
     print("\t-p\tEither 1 or 2 for the part")
     print("\t-h\tPrint this help message\n")
+
+def convert2Int(string):
+    helper = {"one": 1, "two": 2, "three": 3, 
+              "four": 4, "five": 5, "six": 6, 
+              "seven": 7, "eight": 8, "nine": 9}
+    result = helper.get(string)
+    if not result:
+        result = int(string)
+    return result
 
 def main(input_file_name, part):
     lines = []
@@ -16,11 +26,20 @@ def main(input_file_name, part):
     
     # Collect calibration values
     total = 0
+    if part == 1:
+        regex1 = r"([0-9])"
+        regex2 = r"([0-9])"
+    else:
+        regex1 = r"(one|two|three|four|five|six|seven|eight|nine|[0-9])"
+        regex2 = r"(eno|owt|eerht|ruof|evif|xis|neves|thgie|enin|[0-9])"
     for line in lines:
         # Calculate first and last digit, then add number to total
-        numerals = list( filter( lambda i: i.isnumeric(), line ) )
-        first = int( numerals[0] )
-        last = int( numerals[-1] )
+        first = re.search(regex1, line).group()
+        first = convert2Int(first)
+
+        last = re.search(regex2, line[::-1]).group()
+        last = convert2Int(last[::-1])
+
         num = (first * 10) + last
         total += num
     
