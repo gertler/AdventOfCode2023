48a65190303d8385f89915934d2fdc96edeb96fb
Harrison Gertler <harrisongertler@mac.com>
2023-12-04 17:17:44 -0500

    Starting day 4 part 1

---
+++ day4/day4.py
@@ -0,0 +1,49 @@
+#! /usr/bin/env python3
+
+import sys, os
+
+
+def usage():
+    print(f"Usage: {sys.argv[0]} -p [PART_NUMBER] [INPUT_FILE]")
+    print("Part 1: Find the sum of the points for each scratchcard.")
+    print("\t-p\tEither 1 or 2 for the part")
+    print("\t-h\tPrint this help message\n")
+
+
+def main(input_file_name, part):
+    lines = []
+    # Read lines from input
+    with open(input_file_name) as input_file:
+        lines = input_file.readlines()
+
+    points = []
+    
+    if part == 1:
+        print(f"The sum of points on every scratchcard is {sum(points)}")
+
+
+if __name__ == "__main__":
+    # Check args:
+    if "-h" in sys.argv:
+        usage()
+        exit(0)
+    if len(sys.argv) != 4 or sys.argv[1] != "-p":
+        usage()
+        exit(1)
+    
+    # Verify part
+    if not sys.argv[2].isnumeric():
+        usage()
+        exit(1)
+    part = int(sys.argv[2])
+    if part not in [1,2]:
+        usage()
+        exit(1)
+    
+    # Verify input file exists
+    if not os.path.isfile(sys.argv[3]):
+        usage()
+        exit(1)
+    
+    # Go
+    main(sys.argv[3], part)
