e4fd9501711e94a245693605fe3fbe858cbafc8c
Harrison Gertler <harrisongertler@mac.com>
2023-12-12 11:08:25 -0500

    Finished day 10 part 1

--- day10/day10.py
+++ day10/day10.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 
 import sys, os
-import re
+import math
 
 
 def usage():
@@ -12,15 +12,78 @@ def usage():
     print("\t-h\tPrint this help message\n")
 
 
+def nextPipe(grid, curr, prev):
+    conns = []
+    tile = grid[curr[1]][curr[0]]
+    x = curr[0]
+    y = curr[1]
+
+    # Find connecting spaces
+    if tile == "|":
+        conns = [(x, y + dy) for dy in [-1,1]]
+    elif tile == "-":
+        conns = [(x + dx, y) for dx in [-1,1]]
+    elif tile == "L":
+        conns = [(x, y - 1), (x + 1, y)]
+    elif tile == "J":
+        conns = [(x, y - 1), (x - 1, y)]
+    elif tile == "7":
+        conns = [(x - 1, y), (x, y + 1)]
+    elif tile == "F":
+        conns = [(x + 1, y), (x, y + 1)]
+    
+    new = (set(conns) - set([prev])).pop()
+    return new
+
+
+def startPipe(grid, start):
+    x = start[0]
+    y = start[1]
+    if x - 1 >= 0 and grid[y][x-1] in ["-", "L", "J", "7", "F"]:
+        return (x-1, y)
+    elif x + 1 < len(grid[y]) and grid[y][x+1] in ["-", "L", "J", "7", "F"]:
+        return (x+1, y)
+    elif y - 1 >= 0 and grid[y-1][x] in ["|", "L", "J", "7", "F"]:
+        return (x, y-1)
+    else:
+        return (x, y+1)
+
+
+
 def main(input_file_name, part):
     lines = []
     # Read lines from input
     with open(input_file_name) as input_file:
         lines = input_file.readlines()
     
+    # Set the grid and start index
+    grid = []
+    start = None
+    for i in range(len(lines)):
+        g = [x for x in lines[i].strip()]
+        if "S" in g:
+            start = (g.index("S"), i)
+        grid.append(g)
+
+    # Make sure we found a start index
+    if not start:
+        print("Input must have a start index")
+        exit(1)
+    
+    first_pipe = startPipe(grid, start)
+
+    pipe_length = 1
+    curr = nextPipe(grid, first_pipe, start)
+    prev = first_pipe
+    while curr != start:
+        pipe_length += 1
+        temp = curr
+        curr = nextPipe(grid, curr, prev)
+        prev = temp
+
 
     if part == 1:
-        print(f"The sum of the extrapolated values is {0}")
+        print(f"The number of steps in the loop to be farthest from the starting position is {math.ceil(pipe_length / 2)}")
     else:
         print(f"")
 
